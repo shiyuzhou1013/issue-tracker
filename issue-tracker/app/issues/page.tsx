e37767167ea37886353518c1490b6b110446e9c1
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-07-08 15:16:20 +1200

    status badge added

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import IssueStatusBadge from "../components/IssueStatusBadge";
 
 const issuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -30,12 +31,12 @@ const issuesPage = async () => {
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
-              <Table.RowHeaderCell>
+              <Table.Cell>
                 {issue.title}
-                <div className="”block md:hidden”">{issue.status}</div>
-              </Table.RowHeaderCell>
+                <div className="block md:hidden">{issue.status}</div>
+              </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.status}
+                <IssueStatusBadge status={issue.status} />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 {issue.createdAt.toDateString()}
